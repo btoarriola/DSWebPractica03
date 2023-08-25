c5790f9218f8ccc4eb6c3eb3f2f6a5cd162416f0
btoarriola <betokanon@gmail.com>
2023-08-25 09:56:18 -0600

    update initdb

--- initdb.sql
+++ initdb.sql
@@ -1,10 +1,15 @@
 CREATE USER myuser;
 CREATE DATABASE mydb;
 GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
-CREATE TABLE ejemplo(
+ALTER DATABASE mydb OWNER TO myuser;
+
+\connect mydb
+
+CREATE TABLE mytable(
 	clave integer NOT NULL,
 	nombre character varying,
 	direccion character varying,
 	CONSTRAINT pk_clave PRIMARY KEY (clave)
 );
- 
+
+ALTER TABLE mytable OWNER TO myuser;
