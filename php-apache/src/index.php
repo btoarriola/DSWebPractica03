c94abb658b27c67cf5790f77dd2055fc5aae2a77
btoarriola <betokanon@gmail.com>
2023-09-05 11:18:58 -0600

    new apache php

--- php-apache/src/index.php
+++ php-apache/src/index.php
@@ -1,3 +1,39 @@
-<?php
-phpinfo();
-?>
+<html>
+<body>    
+    <?php
+    for($i=0; $i<=10; $i++){
+        echo'hola mundo<br> ';
+    }
+    echo "conecxion a PG";
+    $user="postgres";
+    $password="pass";
+    $url="pgsql:host=172.17.0.2 port=5432 dbname=mydb";
+    try{
+        $conn = new PDO($url,$user, $password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        if($conn){
+            $insert = $conn->prepare("INSERT INTO mytable (clave, nombre, direccion) VALUES (:clave, :nombre, :direccion)");
+            $insert->bindParam(':clave', $_POST['clave']);
+            $insert->bindParam(':nombre', $_POST['nombre']);
+            $insert->bindParam(':direccion', $_POST['direccion']);
+
+            $insert->execute();
+            if($insert){
+                echo "Insert bien";
+            }
+            else{
+                echo "insert mal.";
+            }
+        }
+    }
+    catch(PDOException $exp){
+        echo("No se pudo conectar a la bd, $exp");
+        die($e->getMessage());
+    } finally{
+        if($conn){
+            $conn=null;
+        }
+    }
+
+    ?>
+</body>
+</html>
